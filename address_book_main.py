cb81e9461858af2b4835a5e9111f784233daa1e3
vvivksingh <vvivksingh@gmail.com>
2022-03-04 15:39:28 +0530

    ability to add new contact details

--- address_book_main.py
+++ address_book_main.py
@@ -1,7 +1,24 @@
 from address_book_console import AddressBookConsoleService
 
+ADD_CONTACT = 1
+DISPLAY_CONTACT = 2
+EXIT = 3
+
 if __name__ == "__main__":
     console_service = AddressBookConsoleService()
     print("Welcome to Address Book Management System")
-    contact = console_service.create_contact()
-    console_service.display_contact(contact)
+
+    while True:
+        print(" 1 Add Contact ",
+              "\n 2 Display Contact",
+              "\n 3 Exit")
+        user_choice = int(input())
+        if user_choice == ADD_CONTACT:
+            console_service.add_contact()
+        elif user_choice == DISPLAY_CONTACT:
+            console_service.display_contact()
+        elif user_choice == EXIT:
+            print("Thanks for using us ")
+            exit()
+        else:
+            print("Invalid choice")
