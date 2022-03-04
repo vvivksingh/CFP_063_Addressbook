846208f90c13370676281a0c2b85d0e1284dc4ef
vvivksingh <vvivksingh@gmail.com>
2022-03-04 15:49:18 +0530

    ability to edit contact details

--- address_book_main.py
+++ address_book_main.py
@@ -2,23 +2,33 @@ from address_book_console import AddressBookConsoleService
 
 ADD_CONTACT = 1
 DISPLAY_CONTACT = 2
-EXIT = 3
+EDIT_CONTACT = 3
+EXIT = 0
 
 if __name__ == "__main__":
+
     console_service = AddressBookConsoleService()
     print("Welcome to Address Book Management System")
 
     while True:
         print(" 1 Add Contact ",
               "\n 2 Display Contact",
-              "\n 3 Exit")
+              "\n 3 Edit Contact",
+              "\n 0 Exit")
         user_choice = int(input())
+
         if user_choice == ADD_CONTACT:
             console_service.add_contact()
+
         elif user_choice == DISPLAY_CONTACT:
             console_service.display_contact()
+
+        elif user_choice == EDIT_CONTACT:
+            console_service.edit_contact()
+
         elif user_choice == EXIT:
             print("Thanks for using us ")
             exit()
+
         else:
             print("Invalid choice")
